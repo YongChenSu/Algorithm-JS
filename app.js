a2cda8a864f28c140e61c4b8ef0cc3125bc03d10
YongChenSu <yongchensu0@gmail.com>
2021-07-07 15:24:51 +0800

    Add complexity example

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-function fun1(n) {
-  let ans = 0;
-  for (let i = 1; i <= n; i++) {
-    ans += i;
+function example(n) {
+  let counter = 0;
+  for (let i = 0; i < 3 * n; i++) {
+    counter++;
   }
-  return ans;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      counter++;
+    }
+  }
+
+  counter += 4;
+  return counter;
 }
 
-function fun2(n) {
-  return ((n + 1) * n) / 2;
+for (let i = 2; i < 10; i++) {
+  console.log(`example(${i}) will print out ${example(i)} Hellos.`);
 }
-
-let time1 = window.performance.now();
-fun1(100000000);
-let time2 = window.performance.now();
-let timeDiff1 = (time2 - time1) / 1000;
-console.log(`It takes ${timeDiff1} seconds to run fun1`);
-
-let time3 = window.performance.now();
-fun2(100000000);
-let time4 = window.performance.now();
-let timeDiff2 = (time4 - time3) / 1000;
-console.log(`It takes ${timeDiff2} seconds to run fun2`);
